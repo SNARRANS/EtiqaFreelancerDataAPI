f14024155cc22b45732735408b0f061b98a50a80
Sharul Nizam Ab Rahim <sharulnizam_ar@hotmail.com>
2024-05-19 19:50:29 +0800

    Connect AWS RDS SQL Server

---
+++ LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+namespace EtiqaFreelancerDataAPI
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            base.Init(builder);
+            builder.UseStartup<StartupBase>();
+        }
+    }
+}
