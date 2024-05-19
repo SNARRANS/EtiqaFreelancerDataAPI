bbfc23065358b80701469a75543c89d0e154fe94
Sharul Nizam Ab Rahim <sharulnizam_ar@hotmail.com>
2024-05-19 13:34:50 +0800

    Login Info

--- Models/LoginInfo.cs
+++ Models/LoginInfo.cs
@@ -5,9 +5,7 @@ namespace EtiqaFreelancerDataAPI.Models
 {
     public class LoginInfo
     {
-        //[Key]
-        //[NotMapped]
-        //public required int Id { get; set; }
+      
         public required string Username { get; set; }
         public required string Userpassword { get; set; }
     }
